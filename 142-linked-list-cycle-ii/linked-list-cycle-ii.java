6cb5550eef1ec1bdd1bd7c60a43a1b011e1e320e
vtu26625-Harish <vtu26625@veltech.edu.in>
2026-02-23 09:28:43 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (70.64%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -10,21 +10,44 @@
  * }
  */
 public class Solution {
-    public ListNode detectCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-            if (slow == fast){
-                slow = head;
-                while (slow != fast){
-                    slow = slow.next;
-                    fast = fast.next;
-                }
-                return slow;
+    public ListNode detectCycle(ListNode head)
+    {
+        if(head==null)
+        {
+            return null;
+        }
+
+        if(head.next==null)
+        {
+            return null;
+        }
+        boolean isCycle=false;
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+
+            if(slow==fast)
+            {
+                isCycle=true;
+                break;
             }
         }
-        return null;
+
+        if(!isCycle)
+        {
+            return null;
+        }
+        slow=head;
+        while(slow!=fast)
+        {
+            slow=slow.next;
+            fast=fast.next;
+        }
+
+        return slow;
     }
 }
