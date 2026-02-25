7e39d204468b8a27e6f4659f7485a9ed67c98401
vtu26625-Harish <vtu26625@veltech.edu.in>
2026-02-25 10:28:54 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (25.75%) - LeetSync

---
+++ 898-transpose-matrix/transpose-matrix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[][] trans = new int[col][row];
+
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                trans[i][j] = matrix[j][i];
+            }
+        }
+
+        return trans;
+    }
+}
