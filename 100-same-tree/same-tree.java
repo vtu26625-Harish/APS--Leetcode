d0a933bd2fee3b9c03b9984228d2f4d26a9e4a77
vtu26625-Harish <vtu26625@veltech.edu.in>
2026-04-13 11:07:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (76.01%) - LeetSync

---
+++ 100-same-tree/same-tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return isSameHelp(p,q);
+    }
+    private boolean isSameHelp(TreeNode p, TreeNode q){
+        if(p==null || q==null) return p==q;
+
+        if(p.val != q.val) return false;
+
+        return isSameHelp(p.right, q.right)
+                &&
+                isSameHelp(p.left, q.left);
+    }
+}
